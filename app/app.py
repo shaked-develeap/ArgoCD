fa1ebba2942d33b6f9aad9ab2341a23147edc5db
shaked.dotan <shaked.dotan@stardat.net>
2021-08-22 21:02:57 +0300

    Bootcamp demo

---
+++ app/app.py
@@ -0,0 +1,28 @@
+import os
+from sys import stdout
+from time import sleep
+import datetime
+from flask import Flask, render_template          
+
+
+# MESSAGE="lala land is great!"
+MESSAGE=os.environ['MESSAGE']
+
+app = Flask(__name__)             
+@app.route('/')
+def home():
+    with open(outfile_path, 'a') as outfile:
+        stdout.write(f"{datetime.datetime.now():%d-%m-%Y %H:%m:%S} -- {MESSAGE} with a hard coded message\n")
+        outfile.write(f"{datetime.datetime.now():%d-%m-%Y %H:%m:%S} -- {MESSAGE} with a hard coded message\n")
+        outfile.close
+    with open(outfile_path, 'r') as outfile:        
+        text = outfile.read()
+        return render_template('message.html', message=text)
+
+
+outfile_path="/var/log/python.log"
+# outfile_path="python.log"
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
